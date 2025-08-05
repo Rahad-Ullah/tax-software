421775de847df7a8f53bbf6c636f674d04a099d1
Rahad-Ullah <rahadullah10@gmail.com>
2025-08-05 12:44:38 +0600

    Navbar modified

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,13 +1,53 @@
+"use client";
+
+import { navbarMenus } from "@/constants/navbar-menus";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+
   return (
     <div className="flex justify-between items-center bg-[#F9F9F9] py-4">
       <section>
-        <Image src="/logo.png" alt="Company Logo" width={100} height={24} />
+        <Image
+          src="/logo.png"
+          alt="Company Logo"
+          width={100}
+          height={24}
+          className="rounded-md"
+        />
       </section>
-      <section>Mid navbar</section>
-      <section>Right navbar</section>
+      <section>
+        <ul className="flex gap-8">
+          {navbarMenus.map((item) => (
+            <Link
+              href={item.path}
+              key={item.title}
+              className="px-2 text-[#3D3D3D]"
+            >
+              <li className="flex flex-col items-center">
+                <span>{item.title}</span>
+                {currentPath === item.path && (
+                  <span className="w-10 h-1 bg-primary rounded-full"></span>
+                )}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <Link href={""}>
+          <Image
+            src="/profile.png"
+            alt="Profile Picture"
+            width={50}
+            height={50}
+            className="rounded-full"
+          />
+        </Link>
+      </section>
     </div>
   );
 };
---
+++ src/constants/navbar-menus.ts
@@ -0,0 +1,7 @@
+export const navbarMenus = [
+  { title: "Home", path: "/" },
+  { title: "Deposit", path: "/deposit" },
+  { title: "Income", path: "/income" },
+  { title: "Contact Us", path: "/contact" },
+  { title: "Feedback", path: "/feedback" },
+];
