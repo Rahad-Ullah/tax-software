1d127545b0b120f414e41cc4f7c772d942731e8c
Rahad-Ullah <rahadullah10@gmail.com>
2025-08-05 12:03:39 +0600

    Font family changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <h1 className="text-4xl font-bold">### ONLINE 2024</h1>
+      <h1 className="text-2xl font-bold">Start for FREE</h1>
+    </div>
+  );
 }
